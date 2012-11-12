fc4e36b06898d05a3bde97687f95100b41a01fcb
Arturo Pie <arturotd08@yahoo.ca>
2012-11-11 21:48:47 -0500

    move test of different frameworks to different directories

--- test/minitest/mini2_test.rb
+++ test/minitest/mini2_test.rb
@@ -1,19 +1,12 @@
 require 'minitest/unit'
 
-# require "minitest/reporters"
-# MiniTest::Reporters.use! # MiniTest::Reporters::SpecReporter.new
-
-class GorgonMiniTest < MiniTest::Unit::TestCase
+class GorgonMini2Test < MiniTest::Unit::TestCase
   def test_pass
     assert true
   end
 
   def test_fails
     assert false
-  end
-
-  def test_that_will_be_skipped
-    skip "test this later"
   end
 end
 
