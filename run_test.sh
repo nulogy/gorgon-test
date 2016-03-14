43854cac9a769e0de0bc277a32bdb102b3d59b17
Arturo Pie <arturop@nulogy.com>
2016-03-14 16:45:09 -0400

    replaces username and hostname with placeholder when running ./run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -2,4 +2,4 @@
 
 echo "This script just do a diff on outputs and shows the result of the diff. I know, it's not ideal..."
 
-rm log/test_result.out && gorgon > log/test_result.out && diff correct_test_result.out log/test_result.out
+rm log/test_result.out && gorgon | sed 's/ at .*/ at hostname/g' | sed 's/\/Users\/[^\/]*/Users\/username/g' > log/test_result.out && diff correct_test_result.out log/test_result.out
--- update_correct_test_result.sh
+++ update_correct_test_result.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo "This script updates correct_test_result.out which is used to compare outputs when running 'run_test.sh'"
-rm correct_test_result.out && gorgon > correct_test_result.out
+rm correct_test_result.out && gorgon | sed 's/ at .*/ at hostname/g' | sed 's/\/Users\/[^\/]*/Users\/username/g' > correct_test_result.out
